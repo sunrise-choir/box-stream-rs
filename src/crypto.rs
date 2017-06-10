eacb2a94cc54d612edfaaa0c574752cec7b6af60
AljoschaMeyer <mail@aljoscha-meyer.de>
2017-06-10 17:45:45 +0200

    Add raw bindings

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,34 +1,119 @@
+//! Low-level bindings to box-stream-c. You probably don't need to use this
+//! module directly.
+
 use sodiumoxide::crypto::secretbox;
 
-extern "C" {
-    // TODO doc
-    pub fn bs_encrypt_packet(out: *mut u8,
-                             plain_packet: *const u8,
-                             packet_len: u16,
-                             encryption_key: *const [u8; secretbox::KEYBYTES],
-                             nonce: *mut [u8; secretbox::NONCEBYTES]);
+pub const CYPHER_HEADER_SIZE: usize = secretbox::MACBYTES + 2 + secretbox::MACBYTES;
+pub const MAX_PACKET_SIZE: u16 = 4096;
+
+/// The result of decrypting a cypher_header. This is
+/// `sodiumoxide::crypto::secretbox::MACBYTES` smaller than the encrypted header
+/// since the leading mac is not needed anymore.
+#[repr(C)]
+pub struct PlainHeader {
+    packet_len: u16,
+    packet_mac: [u8; secretbox::MACBYTES],
 }
 
-// // panics if the slice is longer than MAXBYTEs TODO
-// pub fn encrypt_packet(plain_packet: &[u8],
-//                       encryption_key: &secretbox::Key,
-//                       nonce: &mut secretbox::Nonce)
-//                       -> &[u8] {
-//
-// }
+extern "C" {
+    fn bs_encrypt_packet(out: *mut u8,
+                         plain_packet: *const u8,
+                         packet_len: u16,
+                         encryption_key: *const [u8; secretbox::KEYBYTES],
+                         nonce: *mut [u8; secretbox::NONCEBYTES]);
 
-// pub fn ed25519_pk_to_curve25519(ed25519_pub: &sign::PublicKey) -> box_::PublicKey {
-//     let mut curve = [0u8; box_::PUBLICKEYBYTES];
-//     unsafe {
-//         crypto_sign_ed25519_pk_to_curve25519(&mut curve, &ed25519_pub.0);
-//     }
-//     box_::PublicKey(curve)
-// }
-//
-// pub fn ed25519_sk_to_curve25519(ed25519_sec: &sign::SecretKey) -> box_::SecretKey {
-//     let mut curve = [0u8; box_::SECRETKEYBYTES];
-//     unsafe {
-//         crypto_sign_ed25519_sk_to_curve25519(&mut curve, &ed25519_sec.0);
-//     }
-//     box_::SecretKey(curve)
-// }
+    fn bs_final_header(out: *mut [u8; MAX_PACKET_SIZE as usize],
+                       encryption_key: *const [u8; secretbox::KEYBYTES],
+                       nonce: *mut [u8; secretbox::NONCEBYTES]);
+
+
+    fn bs_is_final_header(plain_header: *const PlainHeader) -> bool;
+
+    fn bs_decrypt_header(out: *mut PlainHeader,
+                         cypher_header: *const [u8; CYPHER_HEADER_SIZE],
+                         decryption_key: *const [u8; secretbox::KEYBYTES],
+                         nonce: *mut [u8; secretbox::NONCEBYTES])
+                         -> bool;
+
+    fn bs_decrypt_header_inplace(cypher_header: *mut [u8; CYPHER_HEADER_SIZE],
+                                 decryption_key: *const [u8; secretbox::KEYBYTES],
+                                 nonce: *mut [u8; secretbox::NONCEBYTES])
+                                 -> bool;
+
+    fn bs_decrypt_packet(out: *mut u8,
+                         cypher_packet: *const u8,
+                         plain_header: *const PlainHeader,
+                         decryption_key: *const [u8; secretbox::KEYBYTES],
+                         nonce: *mut [u8; secretbox::NONCEBYTES])
+                         -> bool;
+
+    fn bs_decrypt_packet_inplace(cypher_packet: *mut u8,
+                                 plain_header: *const PlainHeader,
+                                 decryption_key: *const [u8; secretbox::KEYBYTES],
+                                 nonce: *mut [u8; secretbox::NONCEBYTES])
+                                 -> bool;
+}
+
+/// Writes the encrypted header and payload for a given plaintext packet into `out`.
+/// `out` must be a pointer to at least `CYPHER_HEADER_SIZE + packet_len` bytes.
+pub unsafe fn encrypt_packet(out: *mut u8,
+                             plain_packet: *const u8,
+                             packet_len: u16,
+                             encryption_key: &[u8; secretbox::KEYBYTES],
+                             nonce: &mut [u8; secretbox::NONCEBYTES]) {
+    bs_encrypt_packet(out, plain_packet, packet_len, encryption_key, nonce);
+}
+
+/// Writes the final header that signals the end of the box stream into `out`.
+pub unsafe fn final_header(out: &mut [u8; MAX_PACKET_SIZE as usize],
+                           encryption_key: &[u8; secretbox::KEYBYTES],
+                           nonce: &mut [u8; secretbox::NONCEBYTES]) {
+    bs_final_header(out, encryption_key, nonce);
+}
+
+impl PlainHeader {
+    /// Returns whether this header signals the end of the stream.
+    pub fn is_final_header(&self) -> bool {
+        unsafe { bs_is_final_header(self) }
+    }
+}
+
+/// If this returns true, it decrypts a received header into `out`. Returns false
+/// if the cyper_header was invalid.
+pub unsafe fn decrypt_header(out: &mut PlainHeader,
+                             cypher_header: &[u8; CYPHER_HEADER_SIZE],
+                             decryption_key: &[u8; secretbox::KEYBYTES],
+                             nonce: &mut [u8; secretbox::NONCEBYTES])
+                             -> bool {
+    bs_decrypt_header(out, cypher_header, decryption_key, nonce)
+}
+
+/// Same as `decrypt_header`, but writes the result into `cypher_header`. If this
+/// returns true, `cypher_header` can be safely cast to a `PlainHeader`.
+pub unsafe fn decrypt_header_inplace(cypher_header: &mut [u8; CYPHER_HEADER_SIZE],
+                                     decryption_key: &[u8; secretbox::KEYBYTES],
+                                     nonce: &mut [u8; secretbox::NONCEBYTES])
+                                     -> bool {
+    bs_decrypt_header_inplace(cypher_header, decryption_key, nonce)
+}
+
+/// Decrypts a received packet, given a pointer to the corresponding
+/// plain_header, and writes the result into `out`. Returns false on invalid
+/// input, in which case the content of `out` is unspecified.
+pub unsafe fn decrypt_packet(out: *mut u8,
+                             cypher_packet: *const u8,
+                             plain_header: &PlainHeader,
+                             decryption_key: &[u8; secretbox::KEYBYTES],
+                             nonce: &mut [u8; secretbox::NONCEBYTES])
+                             -> bool {
+    bs_decrypt_packet(out, cypher_packet, plain_header, decryption_key, nonce)
+}
+
+/// Same as `decrypt_packet`, but writes the result into `cypher_packet`.
+pub unsafe fn decrypt_packet_inplace(cypher_packet: *mut u8,
+                                     plain_header: &PlainHeader,
+                                     decryption_key: &[u8; secretbox::KEYBYTES],
+                                     nonce: &mut [u8; secretbox::NONCEBYTES])
+                                     -> bool {
+    bs_decrypt_packet_inplace(cypher_packet, plain_header, decryption_key, nonce)
+}
